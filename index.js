f4bdc7284378ee7d77289948f3e5ebc55ada8330
r134x7 <r134x7@gmx.com>
2022-04-30 23:25:25 +1000

    figured out via recursion how to repeat adding team members

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 // will need const for the lib files that take the data from the questions...
 
-const questions = []; // 
+const questions = [
+    "What is the name of the Team Manager?", 
+    "What is their ID number?", 
+    "What is their email address?", 
+    "What is the Team Manager's office number?",
+    "What type of Team Member would you like to add?",
+    "What is the name of the Engineer?",
+    "What is the Engineer's GitHub username?",
+    "What is the name of the Intern?",
+    "What is the name of the Intern's School?"
+    ]; // 
 
-function init() {
+function makeManager() {
 
     inquirer // inquirer is called in the init function
 .prompt([ // prompt method to bring up questions for input
-    
-  ])
-    // .then((data) => writeToFile("parameter1", parameter2) //
+    {
+      type: 'input',
+      name: 'name',
+      message: questions[0],
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: questions[1],
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: questions[2],
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: questions[3],
+    },
+    {
+      type: "list",
+      name: "teamMember",
+      message: questions[4],
+      choices: ['Engineer', 'Intern', 'None, end selection.',],
+    },
+])
+    .then(function (data) {
+        new Manager(data.name, data.id, data.email, data.officeNumber)
+
+        if (data.teamMember === "Engineer"){
+            makeEngineer();
+        } else if (data.teamMember === "Intern"){
+            makeIntern();
+        } else {
+            console.log("oops");
+        }
+    })
+
+    // I have to take the data and put it into the relevant class eg new Intern and then........ I take that input and create a profile and then I repeat the process...............
+    // and probably then the html file is produced probably...
+    // .then((data) => writeToFile("parameter1", parameter2) // 
 }
 
 // function writeToFile(fileName, data) {
@@ -18,4 +72,94 @@ function init() {
     // fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log("HTML file generated.")) // if there is an error, an error is shown on the terminal, else the terminal says file was written successfully.
 // }
 
-init(); 
+function makeEngineer() {
+
+    inquirer // inquirer is called in the init function
+.prompt([ // prompt method to bring up questions for input
+    {
+      type: 'input',
+      name: 'name',
+      message: questions[5],
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: questions[1],
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: questions[2],
+    },
+    {
+      type: "input",
+      name: "username",
+      message: questions[6],
+    },
+    {
+      type: "list",
+      name: "teamMember",
+      message: questions[4],
+      choices: ['Engineer', 'Intern', 'None, end selection.',],
+    },
+])
+    .then(function (data) {
+        new Engineer(data.name, data.id, data.email, data.username)
+        
+        if (data.teamMember === "Engineer"){
+            makeEngineer(); // recursion
+        } else if (data.teamMember === "Intern"){
+            makeIntern(); // go to
+        } else {
+            console.log("oops");
+        }
+
+    })
+}
+
+function makeIntern() {
+
+    inquirer // inquirer is called in the init function
+.prompt([ // prompt method to bring up questions for input
+    {
+      type: 'input',
+      name: 'name',
+      message: questions[7],
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: questions[1],
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: questions[2],
+    },
+    {
+      type: "input",
+      name: "school",
+      message: questions[8],
+    },
+    {
+      type: "list",
+      name: "teamMember",
+      message: questions[4],
+      choices: ['Engineer', 'Intern', 'None, end selection.',],
+    },
+])
+.then(function (data) {
+        new Intern(data.name, data.id, data.email, data.school)
+
+        if (data.teamMember === "Engineer"){
+            makeEngineer(); // go to
+        } else if (data.teamMember === "Intern"){
+            makeIntern(); // recursion
+        } else {
+            console.log("oops");
+        }
+    })
+}
+
+
+makeManager(); 
