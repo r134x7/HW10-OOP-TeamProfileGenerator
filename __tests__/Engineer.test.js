b36926e7e8322fd5d6b2e689b0259de7e474ef7e
r134x7 <r134x7@gmx.com>
2022-04-30 18:01:38 +1000

    fixed up engineer file, added engineer test.

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,44 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+    it("Should return inherited methods: getName", () => {
+        const name = "test";
+
+        const result = new Engineer("test", 1, "test@", "userName").getName();
+
+        expect(result).toEqual(name);
+    })
+
+    it("Should return inherited methods: getId", () => {
+        const id = 1;
+
+        const result = new Engineer("test", 1, "test@", "userName").getId();
+
+        expect(result).toEqual(id);
+    })
+
+    it("Should return inherited methods: getEmail", () => {
+        const email = "test@";
+
+        const result = new Engineer("test", 1, "test@", "userName").getEmail();
+
+        expect(result).toEqual(email);
+    })
+
+    it("Should return github username", () => {
+        const github = "userName";
+        
+        const result = new Engineer("test", 1, "test@", "userName");
+        
+        expect(result.github).toEqual(github);
+    })
+    
+    it("getRole should return the role of the Engineer", () => {
+        const role = "Engineer";
+
+        const result = new Engineer("test", 1, "test@", "userName").getRole();
+    
+        expect(result).toEqual(role);
+    })
+
+});
