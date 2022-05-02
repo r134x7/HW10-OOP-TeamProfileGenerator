55f62ed33305723f1131088dd7bd9a3cba6d06e2
r134x7 <r134x7@gmx.com>
2022-05-02 14:07:53 +1000

    generate html file progression

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,60 +1,102 @@
-// const write = require("../index");
 const fs = require('fs');
 const Engineer = require('../lib/Engineer');
 const Intern = require('../lib/Intern');
 const Manager = require('../lib/Manager');
 
 function generateTeamMembers(data) {
-    // we get the teamMembers array... and then we organise it... and then we put it into the generateHTML somehow... must find way using string literals...
-
-    console.log(data);
 
     const team = [];
-
-    // const team = data;
-    // for (let i = 0; i < data.length; i++) {
-    //     for (const key in data[i]) {
-    //         console.log(` ${i}: ${key} ${data[i][key]}`);
-
-    //     }
-    // }
 
     for (let i = 0; i < data.length; i++) {
         data[i]["name"]
         data[i]["id"]
         data[i]["email"]
         
-        if (data[i]["constructor"]["name"] === "Manager"){
+        if (data[i]["constructor"]["name"] === "Manager"){ // we get the class name to find the role
            var x = data[i]["officeNumber"];
            var y = new Manager().getRole();
+
+            team.push(`
+            <div>
+            ${data[i]["name"]} <br>
+            ${y} <br>
+            <table>
+            <tr>
+            <td>
+            ID: ${data[i]["id"]}
+            </td>
+            </tr>
+            <tr>
+            <td>
+            Email: ${data[i]["email"]}
+            </td>
+            </tr>
+            <tr>
+            <td>
+            Office number: ${x}
+            </td>
+            </tr>
+            </table>
+            </div>`)
+
         } else if (data[i]["constructor"]["name"] === "Engineer"){
            var x = data[i]["github"];
            var y = new Engineer().getRole();
+
+           team.push(`
+           <div>
+           ${data[i]["name"]} <br>
+           ${y} <br>
+           <table>
+           <tr>
+           <td>
+           ID: ${data[i]["id"]}
+           </td>
+           </tr>
+           <tr>
+           <td>
+           Email: ${data[i]["email"]}
+           </td>
+           </tr>
+           <tr>
+           <td>
+           GitHub: ${x}
+           </td>
+           </tr>
+           </table>
+           </div>`)
+
         } else if (data[i]["constructor"]["name"] === "Intern"){
            var x = data[i]["school"];
            var y = new Intern().getRole();
-        }
 
-        team
-        .push(`
-        ${data[i]["name"]}
-        ${data[i]["id"]}
-        ${data[i]["email"]}
-        ${x}
-        ${y}`)
+           team.push(`
+           <div>
+           ${data[i]["name"]} <br>
+           ${y} <br>
+           <table>
+           <tr>
+           <td>
+           ID: ${data[i]["id"]}
+           </td>
+           </tr>
+           <tr>
+           <td>
+           Email: ${data[i]["email"]}
+           </td>
+           </tr>
+           <tr>
+           <td>
+           School: ${x}
+           </td>
+           </tr>
+           </table>
+           </div>`)
+        }
 
     }
 
-    // // console.log(team);
-    // console.log("oops");
-    // console.log(data[0]);
-    // console.log("oops");
-    // console.log(data[1]);
-    // console.log("oops");
-    // console.log(data[0]["name"]);
     return generateHTML(team);
-    // console.log(team)
-    // return 
 }
 
 function generateHTML(data) {
@@ -64,9 +106,13 @@ function generateHTML(data) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Team Profile</title>
     </head>
     <body>
+    <header>
+        <h1>My Team</h1>
+    </header>
     `
 
     const y = `
@@ -74,25 +120,23 @@ function generateHTML(data) {
     </html>
     `
 
-    var t = ``;
+    var t = ``; // need a mutable state
     
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) { // this for loop is to prevent the issue of commas from the array indexes ending up in the html file
         t = t + `
         ${data[i]}
-        `;
+        `; // like i++
     }
 
-    const z = x + t + y;
+    const z = x + t + y; // concatenate
     
-    return writeToFile("index.html", z)
+    return writeToFile("./dist/index.html", z)
 }
 
 function writeToFile(fileName, data) {
 
     fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log("HTML file generated.")) // if there is an error, an error is shown on the terminal, else the terminal says file was written successfully.
 }
-
-// you forgot to export....
 
 module.exports = {
     generateTeamMembers, 
